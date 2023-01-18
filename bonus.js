5aa2c1c19abe29b3c576b83fce46ec2d130ab493
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-01-19 00:49:46 +0100

    added bonus2

--- bonus.js
+++ bonus.js
@@ -1,19 +1,40 @@
+//Bonus 1
 let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in semper leo, in consequat sapien. Vivamus bibendum quis est eu interdum. Nam in commodo sapien. Etiam non efficitur massa. Sed quis fermentum nisl. Vestibulum maximus vulputate metus a iaculis. Aenean ullamcorper condimentum iaculis. Aenean id augue ut lectus dapibus posuere vel in ligula. Donec sodales, nisl ac consectetur malesuada, nisi ante congue ex, a auctor erat sapien vel urna. Curabitur eget congue nibh. Ut id elit vel mi luctus molestie. Vestibulum porttitor viverra orci eu ullamcorper. Nam sed diam ac nunc efficitur lobortis at non diam. Nam condimentum semper enim, et feugiat dui semper eu. Donec convallis eget ante a sodales. Donec ut leo nunc. Mauris laoreet vestibulum sapien, at mattis enim vehicula non. Vestibulum volutpat dui vitae mattis lacinia. Morbi enim eros, tempor non hendrerit at, finibus id dui. Donec imperdiet, diam quis finibus maximus, nulla justo pharetra lorem, id posuere justo purus sit amet quam. Nullam commodo tempor porttitor. Donec sit amet ornare odio. Praesent id tristique dui, at faucibus justo.Suspendisse facilisis nulla at leo feugiat egestas. In id metus vel metus pulvinar laoreet. Morbi magna dolor, efficitur et tempor quis, interdum quis urna. Donec non pellentesque massa, at sagittis ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin pretium, velit laoreet lacinia mattis, nisl elit condimentum mauris, in lobortis mauris lorem sed nisi. Curabitur hendrerit leo nulla, vel scelerisque orci feugiat nec. Aenean non gravida sapien, vel pellentesque mi. Nunc sit amet hendrerit enim, non ornare nisl";
 
 let spaceCount = "";
 
 for (let i = 0; i < longText.length; i++) {    
-if (longText[i] === " ") {
-    spaceCount++
-}
+    if (longText[i] === " ") {
+        spaceCount++;     
+    }
 }
 console.log(spaceCount + 1);
+
 
 let etCount = "";
 
 for (let i = 0; i < longText.length; i++) {
     if (longText[i] === " " && longText[i+1] === "e" && longText[i+2] === "t" && longText[i+3] === " ") {
-etCount++; 
+        etCount++; 
     }
 }
-console.log(etCount)
+console.log(etCount);
+
+//Bonus 2
+
+let phraseToCheck ="amor, roma";
+let reversePhrase="";
+let easyToCheck="";
+
+for (let i = phraseToCheck.length-1; i >=0; i--) {    
+    if(phraseToCheck[i]<="z" && phraseToCheck[i]>="a"){
+        easyToCheck+=phraseToCheck[i];    
+    }
+}
+
+for (let i = easyToCheck.length-1; i >=0; i--) {    
+    reversePhrase+=easyToCheck[i];
+    
+}
+
+console.log(easyToCheck.toUpperCase()===reversePhrase.toUpperCase() ? "It's a palindrome":"It's not a palindrome");
--- index.js
+++ index.js
@@ -1,57 +1,48 @@
 // Iteration 1: Names and Input
-let hacker1 = "leo";
+
+let hacker1 = "Fabien";
 console.log(`The driver's name is ${hacker1}`);
-let hacker2 = "leo";
+let hacker2 = "Delfina";
 console.log(`the navigatore's name is ${hacker2}`);
-
-
-
 
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length}`);
-    
-}
-else if (hacker2.length > hacker1.length) {
-    console.log(`Ìt seems that the navigator has the longesy name, it has ${hacker2.length}`);
-}
-else {
-    console.log(`Wow, you both have equally long names, ${hacker2.length}`);
 
+} else if (hacker2.length > hacker1.length) {
+    console.log(`Ìt seems that the navigator has the longesy name, it has ${hacker2.length}`);
+
+}else {
+    console.log(`Wow, you both have equally long names, ${hacker2.length}`);
 }
 
 // Iteration 3: Loops
-
-let newHacker = "";
+//3.1
+let newHacker1 = "";
 for (let i = 0; i < hacker1.length; i++) {
-
-
-    newHacker = (newHacker + hacker1[i] + " ").toUpperCase();
+    newHacker1 = (newHacker1 + hacker1[i] + " ").toUpperCase();
 }
+console.log(newHacker1);
 
-console.log(newHacker);
-
+//3.2
 let newHacker2 = "";
 for (let i = hacker2.length -1;i >= 0; i--) {
-newHacker2 = (newHacker2 + hacker2[i]);
+    newHacker2 = (newHacker2 + hacker2[i]);
 }
 console.log(newHacker2);
 
-
-
-
+//3.3
 let shortestNameChar = "";
-let shortestName = "";
 
 if (hacker1.length >= hacker2.length) {
     shortestNameChar=hacker2.length;
-}else {
+}else{
     shortestNameChar=hacker1.length;
 } 
 
 if (hacker1 === hacker2){
-        console.log("What ?! You both have the same name?");
+    console.log("What ?! You both have the same name?");
 } else {
 
 for (let i = 0; i < shortestNameChar; i++) {
@@ -63,8 +54,7 @@ for (let i = 0; i < shortestNameChar; i++) {
         console.log("Yo, the navigator goes first definetely.");
         break; 
     } else if (i === shortestNameChar-1) {
-console.log(hacker1.length === shortestName ? "The driver's name goes first.":"Yo, the navigator goes first definetely.")
-        
+        console.log(hacker1.length === shortestName ? "The driver's name goes first.":"Yo, the navigator goes first definetely.");  
     }
 }
 }
